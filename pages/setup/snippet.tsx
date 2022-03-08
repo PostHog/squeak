8df79608958bc456312ec4f4a36f1eeb3725437c
Joe Saunderson <joe.saunderson@mention-me.com>
2022-03-08 18:36:53 +0000

    Mark preflight complete on snippet page

--- pages/setup/snippet.tsx
+++ pages/setup/snippet.tsx
@@ -1,7 +1,11 @@
 import { GetStaticPropsResult, NextPage } from 'next'
-import { withAuthRequired } from '@supabase/supabase-auth-helpers/nextjs'
+import { supabaseServerClient, withAuthRequired } from '@supabase/supabase-auth-helpers/nextjs'
 import styles from '../../styles/Home.module.css'
 import Head from 'next/head'
+import { definitions } from '../../@types/supabase'
+import Link from 'next/link'
+
+type Config = definitions['squeak_config']
 
 interface Props {}
 
@@ -33,7 +37,9 @@ const PreflightSuccess: NextPage<Props> = () => {
                 <h3>Setup complete</h3>
                 <p>Now you can manage users and moderate content in the Squeak! admin portal.</p>
 
-                <button>Visit admin</button>
+                <Link href="/" passHref>
+                    <button>Go to Admin</button>
+                </Link>
             </main>
         </div>
     )
@@ -41,7 +47,11 @@ const PreflightSuccess: NextPage<Props> = () => {
 
 export const getServerSideProps = withAuthRequired({
     redirectTo: '/setup',
-    async getServerSideProps(): Promise<GetStaticPropsResult<Props>> {
+    async getServerSideProps(context): Promise<GetStaticPropsResult<Props>> {
+        const supabaseClient = supabaseServerClient(context)
+
+        await supabaseClient.from<Config>('squeak_config').update({ preflight_complete: true }).match({ id: 1 })
+
         return {
             props: {},
         }
