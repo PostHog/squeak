63b9e21a1664aef10a16fc0a06fa609dad7a35cd
Joe Saunderson <joesaunderson@me.com>
2022-03-11 18:32:03 +0000

    Create channels API

---
+++ pages/api/slack/channels.ts
@@ -0,0 +1,21 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { WebClient } = require('@slack/web-api')
+/* eslint-enable @typescript-eslint/no-var-requires */
+
+const channels = async (req: NextApiRequest, res: NextApiResponse) => {
+    const { token } = req.body
+
+    const client = new WebClient(token + 's')
+
+    try {
+        const result = await client.conversations.list()
+        res.status(200).json(result.channels)
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ error })
+    }
+}
+
+export default channels
