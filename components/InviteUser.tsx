162d577711803896d1e3bcb071a52b2753a02d0d
Joe Saunderson <joesaunderson@me.com>
2022-03-13 21:49:59 +0000

    Add a role selector too

--- components/InviteUser.tsx
+++ components/InviteUser.tsx
@@ -9,12 +9,13 @@ interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 const InviteUser: React.VoidFunctionComponent<Props> = ({ onInvite, className, ...rest }) => {
     const [email, setEmail] = useState('')
     const [firstName, setFirstName] = useState('')
+    const [role, setRole] = useState('admin')
     const [showInput, setShowInput] = useState(false)
 
     const handleInvite = async () => {
         await fetch('/api/user/invite', {
             method: 'POST',
-            body: JSON.stringify({ email, firstName }),
+            body: JSON.stringify({ email, firstName, role }),
         })
 
         if (onInvite) {
@@ -40,6 +41,15 @@ const InviteUser: React.VoidFunctionComponent<Props> = ({ onInvite, className, .
                 placeholder="User's first name"
                 className="inline px-4 py-2 border-gray-300 border rounded-md"
             />
+            <select
+                value={role}
+                onChange={(e) => setRole(e.target.value)}
+                className="inline px-4 py-2 border-gray-300 border rounded-md"
+            >
+                <option value="user">User</option>
+                <option value="moderator">Moderator</option>
+                <option value="admin">Admin</option>
+            </select>
             <Button onClick={handleInvite}>Invite</Button>
         </div>
     ) : (
