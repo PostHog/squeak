0d13f9b1039a2072a5627f9bca2465e5181aafca
Joe Saunderson <joesaunderson@me.com>
2022-03-13 22:41:33 +0000

    Style the login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -2,9 +2,9 @@ import { supabaseClient } from '@supabase/supabase-auth-helpers/nextjs'
 import { Auth } from '@supabase/ui'
 import { GetServerSidePropsContext, GetServerSidePropsResult, NextPage } from 'next'
 import absoluteUrl from 'next-absolute-url'
-import Head from 'next/head'
 import Router from 'next/router'
 import { useEffect } from 'react'
+import Logo from '../components/Logo'
 
 interface Props {
     baseUrl: string
@@ -24,20 +24,34 @@ const Login: NextPage<Props> = () => {
     }, [])
 
     return (
-        <div>
-            <Head>
-                <title>Squeak</title>
-                <meta name="description" content="Something about Squeak here..." />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
+        <div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+            <div className="flex justify-center sm:mx-auto sm:w-full sm:max-w-md">
+                <Logo className="w-40" />
+            </div>
 
-            <main>
-                <h1>Login</h1>
-
-                <Auth supabaseClient={supabaseClient} />
-            </main>
+            <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+                <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+                    <Auth className="login" supabaseClient={supabaseClient} />
+                </div>
+            </div>
         </div>
     )
+
+    // return (
+    //     <div>
+    //         <Head>
+    //             <title>Squeak</title>
+    //             <meta name="description" content="Something about Squeak here..." />
+    //             <link rel="icon" href="/favicon.ico" />
+    //         </Head>
+    //
+    //         <main>
+    //             <h1>Login</h1>
+    //
+    //             <Auth supabaseClient={supabaseClient} />
+    //         </main>
+    //     </div>
+    // )
 }
 
 export const getServerSideProps = ({ req }: GetServerSidePropsContext): GetServerSidePropsResult<Props> => {
