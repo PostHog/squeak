af7ad708e832414a656836b70285f0167959d86f
Joe Saunderson <joesaunderson@me.com>
2022-03-30 18:39:02 +0100

    Use the forms in signup flow

--- pages/signup/profile.tsx
+++ pages/signup/profile.tsx
@@ -7,6 +7,7 @@ import withMultiTenantCheck from '../../util/withMultiTenantCheck'
 import { GetStaticPropsResult } from 'next'
 import { getUser, supabaseServerClient } from '@supabase/supabase-auth-helpers/nextjs'
 import { definitions } from '../../@types/supabase'
+import ProfileForm from '../../components/ProfileForm'
 
 type UserReadonlyProfile = definitions['squeak_profiles_readonly']
 
@@ -39,70 +40,17 @@ const Profile: NextPageWithLayout<Props> = () => {
     }
 
     return (
-        <form className="space-y-6" onSubmit={handleSignup}>
-            {error && <p className="text-center text-sm font-medium text-red-500">{error}</p>}
-            <div>
-                <label htmlFor="firstName" className="block text-sm font-medium text-gray-700">
-                    First name
-                </label>
-                <div className="mt-1">
-                    <input
-                        id="firstName"
-                        name="firstName"
-                        type="text"
-                        value={firstName}
-                        onChange={(e) => setFirstName(e.target.value)}
-                        autoComplete="given-name"
-                        required
-                        className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500 sm:text-sm"
-                    />
-                </div>
-            </div>
-            <div>
-                <label htmlFor="lastName" className="block text-sm font-medium text-gray-700">
-                    Last name
-                </label>
-                <div className="mt-1">
-                    <input
-                        id="lastName"
-                        name="lastName"
-                        type="text"
-                        value={lastName}
-                        onChange={(e) => setLastName(e.target.value)}
-                        autoComplete="family-name"
-                        required
-                        className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500 sm:text-sm"
-                    />
-                </div>
-            </div>
-            <div>
-                <label htmlFor="organization" className="block text-sm font-medium text-gray-700">
-                    Organization name
-                </label>
-                <div className="mt-1">
-                    <input
-                        id="organization"
-                        name="organization"
-                        type="text"
-                        value={organizationName}
-                        onChange={(e) => setOrganizationName(e.target.value)}
-                        autoComplete="organization"
-                        required
-                        className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500 sm:text-sm"
-                    />
-                </div>
-            </div>
-
-            <div>
-                <button
-                    type="submit"
-                    disabled={loading}
-                    className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-orange-500 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
-                >
-                    Complete sign up
-                </button>
-            </div>
-        </form>
+        <ProfileForm
+            handleSignup={handleSignup}
+            error={error}
+            loading={loading}
+            firstName={firstName}
+            setFirstName={setFirstName}
+            lastName={lastName}
+            setLastName={setLastName}
+            organizationName={organizationName}
+            setOrganizationName={setOrganizationName}
+        />
     )
 }
 
