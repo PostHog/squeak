07effe63f0b7035b68181428cecc4498e1644aff
Joe Saunderson <joesaunderson@me.com>
2022-03-30 18:39:02 +0100

    Use the forms in signup flow

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -6,6 +6,7 @@ import type { NextPageWithLayout } from '../../@types/types'
 import Link from 'next/link'
 import { GetStaticPropsResult } from 'next'
 import withMultiTenantCheck from '../../util/withMultiTenantCheck'
+import SignupForm from '../../components/SignupForm'
 
 interface Props {}
 
@@ -34,56 +35,20 @@ const Signup: NextPageWithLayout<Props> = () => {
 
         if (error) {
             setError(error.message)
+            return
         }
     }
 
     return (
-        <form className="space-y-6" onSubmit={handleSignup}>
-            {error && <p className="text-center text-sm font-medium text-red-500">{error}</p>}
-            <div>
-                <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-                    Email address
-                </label>
-                <div className="mt-1">
-                    <input
-                        id="email"
-                        name="email"
-                        type="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        autoComplete="email"
-                        required
-                        className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500 sm:text-sm"
-                    />
-                </div>
-            </div>
-
-            <div>
-                <label htmlFor="password" className="block text-sm font-medium text-gray-700">
-                    Password
-                </label>
-                <div className="mt-1">
-                    <input
-                        id="password"
-                        name="password"
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        autoComplete="current-password"
-                        required
-                        className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500 sm:text-sm"
-                    />
-                </div>
-            </div>
-
-            <div>
-                <button
-                    type="submit"
-                    className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-orange-500 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
-                >
-                    Sign up
-                </button>
-            </div>
+        <>
+            <SignupForm
+                handleSignup={handleSignup}
+                error={error}
+                email={email}
+                setEmail={setEmail}
+                password={password}
+                setPassword={setPassword}
+            />
 
             <div className="text-center text-sm">
                 <Link href="/login" passHref>
@@ -92,7 +57,7 @@ const Signup: NextPageWithLayout<Props> = () => {
                     </a>
                 </Link>
             </div>
-        </form>
+        </>
     )
 }
 
