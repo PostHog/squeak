2060012a83e782914ffedaf14dbf72bb25d363e5
Joe Saunderson <joesaunderson@me.com>
2022-04-04 21:41:15 +0100

    Add CORS to the new questions API

--- pages/api/questions.ts
+++ pages/api/questions.ts
@@ -1,7 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import getQuestions from '../../util/getQuestions'
+import NextCors from 'nextjs-cors'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+    await NextCors(req, res, {
+        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+        origin: '*',
+    })
+
     const params = req.body
     const { data, error } = await getQuestions({ req, res }, params)
 
