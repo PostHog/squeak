c3b38974fcfc325c2bfc250ae94093243b2f6b86
Eli Kinsey <eli@ekinsey.dev>
2022-04-05 19:03:49 -0700

    update nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,15 @@
 module.exports = {
     content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './layout/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
-        colors: { 
-            'light': '#EEEFE9',
-            'accent-light': '#F54E00',
-            'primary-light': '#404040',
-            'gray-light': '#aaa',
-            'white': '#fff',
+        extend: {
+            colors: {
+                light: '#EEEFE9',
+                'accent-light': '#F54E00',
+                'primary-light': '#404040',
+                'gray-light': '#aaa',
+                white: '#fff',
+                red: '#F54E00',
+            },
         },
     },
     plugins: [require('@tailwindcss/forms')],
