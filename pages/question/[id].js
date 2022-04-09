f45bfee9937fe1910b3b206f2e1a1b7b547c6632
Eli Kinsey <eli@ekinsey.dev>
2022-04-09 15:26:34 -0700

    edit button

--- pages/question/[id].js
+++ pages/question/[id].js
@@ -117,8 +117,14 @@ const Question = (props) => {
                     onSubmit={handleModalSubmit}
                 />
             )}
-            <h1>{subject}</h1>
-            <ul className="flex items-center space-x-2 ">
+            <div className="flex items-start space-x-4 justify-between mr-[56px]">
+                <h1 className="m-0">{subject}</h1>
+                <button onClick={() => setModalOpen(true)} className="font-bold text-red">
+                    Edit
+                </button>
+            </div>
+
+            <ul className="flex items-center space-x-2 mb-6">
                 {question.question.slug?.map((slug) => {
                     const url = new URL(domain).origin + slug.trim()
                     return (
@@ -130,9 +136,6 @@ const Question = (props) => {
                     )
                 })}
             </ul>
-            <button onClick={() => setModalOpen(true)} className="mb-6 font-bold text-red">
-                Edit
-            </button>
             <div className="col-span-2">
                 <div className="grid gap-y-4">
                     <div className="flex space-x-9 items-start">
