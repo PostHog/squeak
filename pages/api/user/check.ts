bb0c7a87e64d2b8ecf542be96c9a5e24018859cb
Joe Saunderson <joesaunderson@me.com>
2022-04-12 21:36:45 +0100

    Parse the res body

--- pages/api/user/check.ts
+++ pages/api/user/check.ts
@@ -11,7 +11,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
         origin: '*',
     })
 
-    const { token, organizationId } = req.body
+    const { token, organizationId } = JSON.parse(req.body)
 
     if (!token || !organizationId) {
         res.status(400).json({ error: 'Missing required fields' })
