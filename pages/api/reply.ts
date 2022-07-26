c101dee964349a1a6e2293429c230522464a517c
Alex Sharp <ajsharp@gmail.com>
2022-07-26 02:54:32 +0200

    refactor POST /api/reply to use middlewares, use cookies vs token param
    This is one of many public API endpoints that previously accepted a `token`
    param in the request body, which contained a jwt from supabase.
    
    In the interest of simplifying things, we're no longer using JWTs, and
    instead we're using simple session cookies.

--- pages/api/reply.ts
+++ pages/api/reply.ts
@@ -1,46 +1,39 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import NextCors from 'nextjs-cors'
+import nc from 'next-connect'
 import xss from 'xss'
 import { Prisma, Reply } from '@prisma/client'
 
 import getUserProfile from '../../util/getUserProfile'
 import sendReplyNotification from '../../util/sendReplyNotification'
-import checkAllowedOrigins from '../../util/checkAllowedOrigins'
 import { createReply } from '../../db/reply'
-import { methodNotAllowed } from '../../lib/api/apiUtils'
+import { allowedOrigin, corsMiddleware, validateBody } from '../../lib/middleware'
+import { getSessionUser } from '../../lib/auth'
+import { notAuthenticated, safeJson } from '../../lib/api/apiUtils'
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-    await NextCors(req, res, {
-        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-        origin: '*',
-    })
-
-    const { error: allowedOriginError } = await checkAllowedOrigins(req)
-
-    if (allowedOriginError) {
-        res.status(allowedOriginError.statusCode).json({ error: allowedOriginError.message })
-        return
-    }
-
-    switch (req.method) {
-        case 'POST':
-            return await handleCreate(req, res)
-        default:
-            return methodNotAllowed(res)
-    }
+const schema = {
+    type: 'object',
+    properties: {
+        messageId: { type: 'number' },
+        organizationId: { type: 'string' },
+        body: { type: 'string' },
+    },
+    required: ['messageId', 'organizationId', 'body'],
 }
+
+const handler = nc<NextApiRequest, NextApiResponse>()
+    .use(corsMiddleware)
+    .use(allowedOrigin)
+    .post(validateBody(schema, { coerceTypes: true }), handleCreate)
 
 // POST /api/reply
 // Public endpoint to create a reply
 async function handleCreate(req: NextApiRequest, res: NextApiResponse) {
-    const { messageId, organizationId, token } = JSON.parse(req.body)
+    const { messageId, organizationId, body: rawBody } = req.body
 
-    if (!messageId || !JSON.parse(req.body).body || !organizationId || !token) {
-        res.status(400).json({ error: 'Missing required fields' })
-        return
-    }
+    const user = await getSessionUser(req)
+    if (!user) return notAuthenticated(res)
 
-    const body = xss(JSON.parse(req.body).body, {
+    const body = xss(rawBody, {
         whiteList: {},
         stripIgnoreTag: true,
     })
@@ -48,7 +41,7 @@ async function handleCreate(req: NextApiRequest, res: NextApiResponse) {
     const { data: userProfile, error: userProfileError } = await getUserProfile({
         context: { req, res },
         organizationId,
-        token,
+        user,
     })
 
     if (!userProfile || userProfileError) {
@@ -65,7 +58,7 @@ async function handleCreate(req: NextApiRequest, res: NextApiResponse) {
 
     try {
         const reply: Reply = await createReply(organizationId, body, messageId, userProfile)
-        res.status(201).json(reply)
+        safeJson(res, reply, 201)
         sendReplyNotification(organizationId, messageId, body)
     } catch (err) {
         if (err instanceof Prisma.PrismaClientKnownRequestError) {
