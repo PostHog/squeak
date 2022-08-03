4627855c5606769a852d01c169448463f3ee50db
Alex Sharp <ajsharp@gmail.com>
2022-08-03 20:05:38 +0200

    wire up logout buttons to rest api

--- components/LogoutButton.tsx
+++ components/LogoutButton.tsx
@@ -1,22 +1,10 @@
-import { supabaseClient } from '@supabase/supabase-auth-helpers/nextjs'
-import { useEffect } from 'react'
 import Router from 'next/router'
+import { logout } from '../lib/api/auth'
 
 const LogoutButton = () => {
-    useEffect(() => {
-        const { data: subscription } = supabaseClient.auth.onAuthStateChange((event) => {
-            if (event === 'SIGNED_OUT') {
-                Router.push('/login')
-            }
-        })
-
-        return () => {
-            subscription?.unsubscribe()
-        }
-    }, [])
-
     const handleLogout = async () => {
-        await supabaseClient.auth.signOut()
+        await logout()
+        Router.push('/login')
     }
 
     return <button onClick={handleLogout}>Logout</button>
