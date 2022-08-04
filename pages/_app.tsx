fb4ba59611fe65b07dc5f738529c4b3a4f457e91
Alex Sharp <ajsharp@gmail.com>
2022-08-04 03:14:42 +0300

    replace supabase user provider with internal

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import '@fontsource/nunito'
 import '@fontsource/nunito/600.css'
 import '@fontsource/nunito/800.css'
-import { supabaseClient } from '@supabase/supabase-auth-helpers/nextjs'
-import { UserProvider } from '@supabase/supabase-auth-helpers/react'
+
 import type { AppProps } from 'next/app'
 import { ToastProvider } from 'react-toast-notifications'
+
 import type { NextPageWithLayout } from '../@types/types'
 import Toast from '../components/Toast'
+import { UserProvider } from '../contexts/user'
 import ErrorLayout from '../layout/ErrorLayout'
 import '../styles/globals.css'
 
@@ -22,7 +23,7 @@ function Squeak({ Component, pageProps }: AppPropsWithLayout) {
     const getLayout = Component.getLayout || ((page) => page)
 
     return (
-        <UserProvider supabaseClient={supabaseClient}>
+        <UserProvider user={pageProps.user}>
             <ToastProvider newestOnTop autoDismiss components={{ Toast: Toast }}>
                 {getLayout(<Component {...pageProps} />)}
             </ToastProvider>
