d033fe60496caae0bba51e6b34ff187d5597e856
Alex Sharp <ajsharp@gmail.com>
2022-08-08 16:28:01 -0700

    enable prisma logging

--- lib/db.ts
+++ lib/db.ts
@@ -6,10 +6,14 @@ import { PrismaClient } from '@prisma/client'
 let prisma: PrismaClient
 
 if (process.env.NODE_ENV === 'production') {
-    prisma = new PrismaClient()
+    prisma = new PrismaClient({
+        log: ['query', 'info', 'warn', 'error'],
+    })
 } else {
     if (!global.prisma) {
-        global.prisma = new PrismaClient()
+        global.prisma = new PrismaClient({
+            log: ['query', 'info', 'warn', 'error'],
+        })
     }
     prisma = global.prisma
 }
