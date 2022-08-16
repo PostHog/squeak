a86e0fdccdc9fd86f299bad52e60c089573994cb
Alex Sharp <ajsharp@gmail.com>
2022-08-15 19:01:20 -0700

    minor refactor to simplify function

--- db/profiles.ts
+++ db/profiles.ts
@@ -26,20 +26,19 @@ export interface CreateProfileParams {
  */
 export async function findOrCreateProfileFromSlackUser(params: CreateProfileParams) {
     const { slack_user_id, first_name, last_name, avatar, organization_id } = params
-    let profileId
     const data = await prisma.profileReadonly.findFirst({
         where: { slack_user_id },
         select: { profile_id: true },
     })
 
     if (data?.profile_id) {
-        profileId = data.profile_id
-    } else {
-        // Create a new profile for the slack user
-        const { data: profile } = await createUserProfile({ first_name, last_name, avatar })
-
-        await createUserProfileReadonly(null, organization_id, profile?.id || '', 'user', slack_user_id)
-        profileId = profile?.id
+        return data.profile_id
     }
-    return profileId
+
+    // Create a new profile for the slack user
+    const { data: profile } = await createUserProfile({ first_name, last_name, avatar })
+
+    await createUserProfileReadonly(null, organization_id, profile?.id || '', 'user', slack_user_id)
+
+    return profile?.id
 }
