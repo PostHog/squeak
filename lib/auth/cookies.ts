9169040950db4582ca2c283375d4ed97e464e367
Alex Sharp <ajsharp@gmail.com>
2022-08-17 18:46:26 -0700

    set cookie domain based on the originating request origin

--- lib/auth/cookies.ts
+++ lib/auth/cookies.ts
@@ -1,4 +1,5 @@
 import { serialize, parse } from 'cookie'
+import { IncomingMessage } from 'http'
 import { NextApiResponse } from 'next'
 import { RequestWithCookies } from './session'
 
@@ -6,7 +7,14 @@ export const TOKEN_NAME = 'squeak_session'
 
 export const MAX_AGE = 86400 * 30 // 30 days
 
+function cookieDomainFromOrigin(req: IncomingMessage) {
+    const origin = req.headers.origin || 'squeak.cloud'
+    return origin.replace(/^https?:\/\//, '').replace(/\:\d+$/, '')
+}
+
 export function setTokenCookie(res: NextApiResponse, token: string) {
+    const domain = cookieDomainFromOrigin(res.req)
+
     const cookie = serialize(TOKEN_NAME, token, {
         maxAge: MAX_AGE,
         expires: new Date(Date.now() + MAX_AGE * 1000),
@@ -14,6 +22,7 @@ export function setTokenCookie(res: NextApiResponse, token: string) {
         secure: process.env.NODE_ENV === 'production',
         path: '/',
         sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+        domain: domain,
     })
 
     res.setHeader('Set-Cookie', cookie)
