5639ddc2a1194f7026b2e8e5dc02bdd61eb1e969
Eli Kinsey <eli@ekinsey.dev>
2022-08-28 16:11:38 -0700

    add first and last name to user endpoint

--- pages/api/user.ts
+++ pages/api/user.ts
@@ -7,25 +7,26 @@ import prisma from '../../lib/db'
 import { corsMiddleware } from '../../lib/middleware'
 import { notAuthenticated, safeJson } from '../../lib/api/apiUtils'
 import getActiveOrganization from '../../util/getActiveOrganization'
-import { ProfileReadonly, User } from '@prisma/client'
+import { ProfileReadonly, User, Profile } from '@prisma/client'
 
 export interface GetUserResponse {
     id: string
     email: string | null
     role: string | null
     isModerator: boolean
-    profile: ProfileReadonly
+    profile: ProfileReadonly & Pick<Profile, 'first_name' | 'last_name'>
 }
 
 export function safeSerializeUser(
     user: Pick<User, 'id' | 'email' | 'role'>,
-    profile: ProfileReadonly
+    profile: ProfileReadonly,
+    publicProfile: Pick<Profile, 'first_name' | 'last_name'>
 ): GetUserResponse {
     return {
         id: user.id,
         email: user.email,
         role: user.role,
-        profile,
+        profile: { ...profile, ...publicProfile },
         isModerator: profile.role === 'moderator' || profile.role === 'admin',
     }
 }
@@ -58,7 +59,13 @@ const handler = nextConnect<NextApiRequest, NextApiResponse>()
         if (!user) return res.status(401).json({ error: 'Not authenticated' })
 
         const profile = user.squeak_profiles_readonly && user.squeak_profiles_readonly[0]
-        safeJson(res, safeSerializeUser(user, profile))
+        const publicProfile = await prisma.profile.findUnique({
+            where: { id: profile.profile_id },
+        })
+
+        if (!publicProfile) return res.status(401).json({ error: 'Profile not found' })
+
+        safeJson(res, safeSerializeUser(user, profile, publicProfile))
     })
 
 export default handler
