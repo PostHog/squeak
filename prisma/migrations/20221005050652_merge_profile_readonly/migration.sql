38767e453a8576f811112a9739175df8ef37f6f3
Paul Hultgren <pjhultgren5@gmail.com>
2022-10-04 23:40:07 -0700

    create 20221005050652_merge_profile_readonly migration

---
+++ prisma/migrations/20221005050652_merge_profile_readonly/migration.sql
@@ -0,0 +1,43 @@
+BEGIN;
+
+ALTER TABLE "squeak_profiles"
+    ADD COLUMN     "organization_id" UUID,
+    ADD COLUMN     "role" TEXT NOT NULL DEFAULT 'user',
+    ADD COLUMN     "slack_user_id" TEXT,
+    ADD COLUMN     "team_id" BIGINT,
+    ADD COLUMN     "user_id" UUID;
+
+UPDATE squeak_profiles as profile SET
+    team_id = merged_profile.team_id,
+    organization_id = merged_profile.organization_id,
+    slack_user_id = merged_profile.slack_user_id,
+    user_id = merged_profile.user_id,
+    role = merged_profile.role
+FROM (
+    SELECT
+        squeak_profiles.id::uuid id,
+        readonly.user_id user_id,
+        readonly.organization_id,
+        COALESCE(readonly.role, 'user') role,
+        readonly.slack_user_id,
+        readonly."teamId" team_id
+    FROM squeak_profiles_readonly readonly
+        FULL OUTER JOIN squeak_profiles on squeak_profiles.id = readonly.profile_id
+) AS merged_profile
+WHERE merged_profile.id = profile.id;
+
+-- Add foreign key constraints
+ALTER TABLE "squeak_profiles" ADD CONSTRAINT "squeak_profiles_organization_id_fkey" FOREIGN KEY ("organization_id") REFERENCES "squeak_organizations"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE "squeak_profiles" ADD CONSTRAINT "squeak_profiles_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE "squeak_profiles" ADD CONSTRAINT "squeak_profiles_team_id_fkey" FOREIGN KEY ("team_id") REFERENCES "squeak_teams"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+COMMIT;
+
+-- Drop the squeak_profiles_readonly table
+
+ALTER TABLE "squeak_profiles_readonly" DROP CONSTRAINT "profiles_readonly_organizations_id_fkey";
+ALTER TABLE "squeak_profiles_readonly" DROP CONSTRAINT "profiles_readonly_profile_id_fkey";
+ALTER TABLE "squeak_profiles_readonly" DROP CONSTRAINT "profiles_readonly_user_id_fkey";
+ALTER TABLE "squeak_profiles_readonly" DROP CONSTRAINT "squeak_profiles_readonly_teamId_fkey";
+
+DROP TABLE "squeak_profiles_readonly";
