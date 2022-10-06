db27e4bf0043117e160fa7d0de7ba6c68aedc6cd
Paul Hultgren <pjhultgren5@gmail.com>
2022-10-06 14:02:01 -0700

    fix build

--- src/components/QuestionsTable.tsx
+++ src/components/QuestionsTable.tsx
@@ -121,9 +121,9 @@ export const QuestionsTable: React.FC<QuestionsTableProps> = ({
                                                             viewBox="0 0 24 24"
                                                             fill="none"
                                                             stroke="currentColor"
-                                                            stroke-width="2"
-                                                            stroke-linecap="round"
-                                                            stroke-linejoin="round"
+                                                            strokeWidth="2"
+                                                            strokeLinecap="round"
+                                                            strokeLinejoin="round"
                                                             className="w-4 h-4"
                                                         >
                                                             <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z" />
