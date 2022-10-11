b4fc5602287b447c7c9719a4b5671af9cb93eaee
Paul Hultgren <39424187+pjhul@users.noreply.github.com>
2022-10-11 16:19:16 -0400

    drop squeak_replies_feedback table (#231)

---
+++ prisma/migrations/20221010220702_drop_replies_feedback/migration.sql
@@ -0,0 +1,17 @@
+/*
+  Warnings:
+
+  - You are about to drop the `squeak_replies_feedback` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "squeak_replies_feedback" DROP CONSTRAINT "replies_feedback_organization_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "squeak_replies_feedback" DROP CONSTRAINT "replies_feedback_profile_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "squeak_replies_feedback" DROP CONSTRAINT "replies_feedback_reply_id_fkey";
+
+-- DropTable
+DROP TABLE "squeak_replies_feedback";
