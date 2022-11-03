394af100b8d673ba9e2c9b7d275a1dce9c89657a
Paul Hultgren <39424187+pjhul@users.noreply.github.com>
2022-11-03 14:33:31 -0500

    feat: expanded profiles (#239)
    * remove useActiveOrganization
    
    * add new fields to profiles

---
+++ prisma/migrations/20221103191831_add_profile_fields/migration.sql
@@ -0,0 +1,10 @@
+-- AlterTable
+ALTER TABLE "squeak_profiles" ADD COLUMN     "biography" TEXT,
+ADD COLUMN     "company" TEXT,
+ADD COLUMN     "company_role" TEXT,
+ADD COLUMN     "email" TEXT,
+ADD COLUMN     "github" TEXT,
+ADD COLUMN     "linkedin" TEXT,
+ADD COLUMN     "location" TEXT,
+ADD COLUMN     "twitter" TEXT,
+ADD COLUMN     "website" TEXT;
