6297ab1c6d244189ee65f330ffccf33e76d6ecae
Paul Hultgren <pjhultgren5@gmail.com>
2022-11-09 09:07:51 -0800

    move to monorepo

--- packages/squeak/sentry.client.config.js
+++ packages/squeak/sentry.client.config.js
@@ -1,5 +1,5 @@
-// This file configures the initialization of Sentry on the server.
-// The config you add here will be used whenever the server handles a request.
+// This file configures the initialization of Sentry on the browser.
+// The config you add here will be used whenever a page is visited.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from '@sentry/nextjs'
@@ -9,7 +9,7 @@ const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
 Sentry.init({
     dsn: SENTRY_DSN,
     // Adjust this value in production, or use tracesSampler for greater control
-    tracesSampleRate: 0.0001  // 0.001%
+    tracesSampleRate: 0.0001, // 0.001%
     // ...
     // Note: if you want to override the automatic release value, do not set a
     // `release` value here - use the environment variable `SENTRY_RELEASE`, so
