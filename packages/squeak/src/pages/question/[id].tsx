fb5a2ab5079e2acd557c2e04e8df48b33d9a1bfc
Paul Hultgren <pjhultgren5@gmail.com>
2022-11-09 23:09:33 -0800

    dynamic import

--- packages/squeak/src/pages/question/[id].tsx
+++ packages/squeak/src/pages/question/[id].tsx
@@ -10,15 +10,14 @@ import { withAdminGetStaticProps } from '../../util/withAdminAccess'
 import { getConfig } from '../../db'
 import prisma from 'src/lib/db'
 import dynamic from 'next/dynamic'
-import { Question as SqueakQuestion } from '../../../../squeak-react/src/index'
 
-/*const SqueakQuestion = dynamic(
+const SqueakQuestion: any = dynamic(
     // @ts-ignore
     import('squeak-react').then((mod) => mod.Question),
     {
         ssr: false,
     }
-)*/
+)
 
 const Question = ({ question, topics: allTopics, organizationId, permalinkBase }) => {
     const router = useRouter()
