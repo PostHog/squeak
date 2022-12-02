ab98e51296c9b4010e88d874f05290f0bca30c69
Eli Kinsey <eli@ekinsey.dev>
2022-12-02 08:34:14 -0800

    Update packages/squeak/src/pages/api/roadmap/unsubscribe.ts
    Co-authored-by: Paul Hultgren <39424187+pjhul@users.noreply.github.com>

--- packages/squeak/src/pages/api/roadmap/unsubscribe.ts
+++ packages/squeak/src/pages/api/roadmap/unsubscribe.ts
@@ -18,7 +18,7 @@ async function unsubscribeFromRoadmapItem(req: NextApiRequest, res: NextApiRespo
 
     const { id } = req.body
 
-    if (!id) return res.status(500).json({ error: 'No roadmap ID provided' })
+    if (!id) return res.status(400).json({ error: 'No roadmap ID provided' })
 
     await prisma.roadmap
         .update({
