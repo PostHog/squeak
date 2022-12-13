e6b4a8c27f557249386b1edee6fe896515c002fa
Paul Hultgren <pjhultgren5@gmail.com>
2022-12-12 20:28:03 -0800

    allow filtering based on permalink

--- packages/squeak/src/pages/api/v1/questions/index.ts
+++ packages/squeak/src/pages/api/v1/questions/index.ts
@@ -22,103 +22,108 @@ async function fetchQuestions(req: NextApiRequest, res: NextApiResponse) {
         topic,
         profileId,
         sortBy = 'newest', // 'newest' |  'activity' | 'popular'
+        permalink,
     } = req.query as Record<string, string>
 
     const queryConditions: Prisma.QuestionWhereInput = {
         organization_id: organizationId,
     }
 
-    if (published) queryConditions.published = published === 'true'
-    if (slug) queryConditions.slug = { has: slug }
-    if (profileId)
-        queryConditions.replies = {
-            some: {
-                profile_id: profileId,
-            },
-        }
-    if (topic) {
-        queryConditions.topics = {
-            some: {
-                topic_id: BigInt(topic),
-            },
-        }
-    }
-
     const orderBy: Prisma.QuestionOrderByWithRelationInput = {}
 
-    if (sortBy === 'newest') {
-        orderBy.created_at = 'desc'
-    } else if (sortBy === 'popular') {
-        orderBy.replies = {
-            _count: 'desc',
+    if (permalink) {
+        queryConditions.permalink = permalink
+    } else {
+        if (published) queryConditions.published = published === 'true'
+        if (slug) queryConditions.slug = { has: slug }
+        if (profileId)
+            queryConditions.replies = {
+                some: {
+                    profile_id: profileId,
+                },
+            }
+        if (topic) {
+            queryConditions.topics = {
+                some: {
+                    topic_id: BigInt(topic),
+                },
+            }
         }
-    } else if (sortBy === 'activity') {
-        const latestQuestions: { id: bigint }[] = await prisma.$queryRaw`
+
+        if (sortBy === 'newest') {
+            orderBy.created_at = 'desc'
+        } else if (sortBy === 'popular') {
+            orderBy.replies = {
+                _count: 'desc',
+            }
+        } else if (sortBy === 'activity') {
+            const latestQuestions: { id: bigint }[] = await prisma.$queryRaw`
             SELECT id FROM squeak_messages LEFT JOIN (
                 SELECT message_id, max(created_at) AS latest_reply_ts FROM squeak_replies GROUP BY message_id
             ) AS latest_replies
             ON "squeak_messages".id = latest_replies.message_id
             WHERE (
                 "public"."squeak_messages"."organization_id"::text = ${organizationId} AND "public"."squeak_messages"."published" = ${
-            published === 'true'
-        }
+                published === 'true'
+            }
             )
             ORDER BY latest_reply_ts DESC
             OFFSET ${BigInt(start)}
             LIMIT ${BigInt(perPage)}
         `
 
-        const messages = await prisma.question.findMany({
-            where: {
-                id: {
-                    in: latestQuestions.map((q) => q.id),
-                },
-            },
-            include: {
-                _count: {
-                    select: {
-                        replies: true,
+            const messages = await prisma.question.findMany({
+                where: {
+                    id: {
+                        in: latestQuestions.map((q) => q.id),
                     },
                 },
-                replies: {
-                    include: {
-                        profile: true,
+                include: {
+                    _count: {
+                        select: {
+                            replies: true,
+                        },
                     },
-                    orderBy: { created_at: 'asc' },
-                },
-                topics: {
-                    include: {
-                        topic: true,
+                    replies: {
+                        include: {
+                            profile: true,
+                        },
+                        orderBy: { created_at: 'asc' },
+                    },
+                    topics: {
+                        include: {
+                            topic: true,
+                        },
                     },
                 },
-            },
-        })
+            })
 
-        const orderMap = latestQuestions.reduce((acc, q, i) => {
-            acc[q.id.toString()] = i
-            return acc
-        }, {})
+            const orderMap = latestQuestions.reduce((acc, q, i) => {
+                acc[q.id.toString()] = i
+                return acc
+            }, {})
 
-        let sortedMessages = messages.sort((a, b) => {
-            return orderMap[a.id.toString()] - orderMap[b.id.toString()]
-        })
+            let sortedMessages = messages.sort((a, b) => {
+                return orderMap[a.id.toString()] - orderMap[b.id.toString()]
+            })
 
-        const data = {
-            questions: sortedMessages.map((message) => {
-                const { _count, ...question } = message
-                const profile = message.replies?.[0]?.profile
+            const data = {
+                questions: sortedMessages.map((message) => {
+                    const { _count, ...question } = message
+                    const profile = message.replies?.[0]?.profile
 
-                return {
-                    ...question,
-                    profile,
-                    numReplies: _count.replies,
-                }
-            }),
-            count: messages.length,
+                    return {
+                        ...question,
+                        profile,
+                        numReplies: _count.replies,
+                    }
+                }),
+                count: messages.length,
+            }
+
+            safeJson(res, data)
+            return
         }
-
-        safeJson(res, data)
-        return
     }
 
     const messages = await prisma.question.findMany({
