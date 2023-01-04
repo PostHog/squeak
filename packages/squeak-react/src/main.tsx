8868f6de8e62ad229884d09651cf495dbf454947
Paul Hultgren <pjhultgren5@gmail.com>
2023-01-04 12:56:35 -0800

    update demo component to Squeak

--- packages/squeak-react/src/main.tsx
+++ packages/squeak-react/src/main.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Question } from './index'
+import { Squeak } from './index'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Question
+    <Squeak
       apiHost='http://localhost:3000'
       organizationId=''
       onSubmit={() => {}}
-      onResolve={() => {}}
+      onSignUp={() => {}}
+      onLoad={() => {}}
     />
   </React.StrictMode>
 )
