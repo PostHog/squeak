a0579cca4a8f6cbe53925645818ac1864e1230fb
Paul Hultgren <pjhultgren5@gmail.com>
2023-03-20 17:01:59 -0500

    update formData import and change back node version

--- packages/squeak/src/lib/email/client.ts
+++ packages/squeak/src/lib/email/client.ts
@@ -1,7 +1,7 @@
 import Mailgun from 'mailgun.js'
-import formData from 'form-data'
+import * as formData from 'form-data'
 
-const mailgun = new Mailgun(formData)
+const mailgun = new Mailgun(formData.default)
 
 async function getClient(mailgunApiKey: string) {
     return mailgun.client({
